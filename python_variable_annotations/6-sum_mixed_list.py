4b511595320ee755f5abec7e8c52afe7288730f2
fsl5i <busbar51.com@gmail.com>
2026-02-18 08:27:22 +0000

    Add sum_mixed_list function with List[Union[int, float]] annotation

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that provides a function to sum a list of integers and floats."""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of a list containing integers and floats."""
+    return sum(mxd_lst)
