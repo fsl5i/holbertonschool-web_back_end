930080126d8276808d175a881f871262b0a8597a
faisal <busbar51.com@gmail.com>
2026-02-20 19:40:53 +0000

    Add TypeVar annotations for safely_get_value

---
+++ python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Add type annotations using TypeVar for safely_get_value."""
+
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')  # generic type for default and return
+
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Return the value for key if present, else return default."""
+    if key in dct:
+        return dct[key]
+    return default
