0332295eeccdabbd4ef328063afd79ee73f17642
fsl5i <busbar51.com@gmail.com>
2026-04-23 15:20:47 +0000

    fix

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,15 +1,10 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import createEmployeesObject from './11-createEmployeesObject.js';
 
-export default function getFullBudget(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
-  return fullBudget;
 }
