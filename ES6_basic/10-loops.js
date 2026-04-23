c1c1f45a2ecfc462baa8558aef79b4dc28636899
fsl5i <busbar51.com@gmail.com>
2026-04-23 15:15:22 +0000

    fix

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,15 +1,7 @@
-import getBudgetObject from './7-getBudgetObject.js';
-
-export default function getFullBudget(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
-  return fullBudget;
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+  return newArray;
 }
